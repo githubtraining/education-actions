a7416eaa458e682e9a2f4010b951cad196d22d3f
mattdavis0351 <mattdavis0351@github.com>
2020-05-20 09:53:18 -0700

    remove dev null

--- course-envs/actions-poc-python/entrypoint.sh
+++ course-envs/actions-poc-python/entrypoint.sh
@@ -3,6 +3,6 @@
 mv "$GITHUB_WORKSPACE/$2" /tests
 
 ERROR_FILE=$(cat /errors/$1.md)
-echo "::set-output name=error::$ERROR_FILE" >> /devl/null
+echo "::set-output name=error::$ERROR_FILE"
 
 pytest /tests/$1.py
