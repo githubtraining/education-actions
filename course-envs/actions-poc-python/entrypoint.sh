b4e0cf4d37d4498587ad515fcf686d36429c51f9
mattdavis0351 <mattdavis0351@github.com>
2020-05-20 21:18:42 -0700

    print error to screen

--- course-envs/actions-poc-python/entrypoint.sh
+++ course-envs/actions-poc-python/entrypoint.sh
@@ -17,16 +17,21 @@ mv "$GITHUB_WORKSPACE/$2" /tests
 # -X POST -d '{"body": "${ERROR_FILE}"}' \
 # "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments"
 
-ERROR_FILE="$(</errors/$1.md)"
-echo $ERROR_FILE
-ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
-ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
-ERROR_FILE="${ERROR_FILE//$'\r'/'%0D'}"
+# ERROR_FILE="$(</errors/$1.md)"
+# echo $ERROR_FILE
+# ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
+# ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
+# ERROR_FILE="${ERROR_FILE//$'\r'/'%0D'}"
 
-echo "::set-output name=error::$ERROR_FILE"
+# echo "::set-output name=error::$ERROR_FILE"
 
 
 pytest /tests/$1.py
+
+if [[ $? == 0 ]]; then
+    cat /errors$1.md
+    exit 0
+fi
 
 
 
