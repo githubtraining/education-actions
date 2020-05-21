3027f5e14ca03154dfb67347ba014e44d4f13e16
mattdavis0351 <mattdavis0351@github.com>
2020-05-20 18:01:40 -0700

    last test for the evening

--- course-envs/actions-poc-python/entrypoint.sh
+++ course-envs/actions-poc-python/entrypoint.sh
@@ -1,11 +1,12 @@
-#!/bin/sh -l
+#!/bin/bash
 
 mv "$GITHUB_WORKSPACE/$2" /tests
 
-ERROR_FILE="$(cat /errors/$1.md)"
-# ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
-# ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
-# ERROR_FILE="${ERROR_FILE//$'\r'/'%0D'}"
+ERROR_FILE="$(</errors/$1.md)"
+echo ERROR_FILE
+ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
+ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
+ERROR_FILE="${ERROR_FILE//$'\r'/'%0D'}"
 
 echo "::set-output name=error::$ERROR_FILE"
 
