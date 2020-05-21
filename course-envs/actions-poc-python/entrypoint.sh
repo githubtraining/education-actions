8fb842741bbe1295380b9fd55e71d8bd1fd77020
mattdavis0351 <mattdavis0351@github.com>
2020-05-20 17:35:49 -0700

    multiline support

--- course-envs/actions-poc-python/entrypoint.sh
+++ course-envs/actions-poc-python/entrypoint.sh
@@ -2,7 +2,10 @@
 
 mv "$GITHUB_WORKSPACE/$2" /tests
 
-ERROR_FILE=$(cat /errors/$1.md)
+ERROR_FILE="$(cat /errors/$1.md)"
+ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
+ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
+ERROR_FILE="${ERROR_FILE//$'\r'/'%0D'}"
 
 echo "::set-output name=error::$ERROR_FILE"
 
