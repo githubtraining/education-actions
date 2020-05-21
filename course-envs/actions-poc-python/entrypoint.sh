e9adefbb9963cc1e950ac6e7aa1d0403ff38e9df
mattdavis0351 <mattdavis0351@github.com>
2020-05-20 18:28:36 -0700

    curl test

--- course-envs/actions-poc-python/entrypoint.sh
+++ course-envs/actions-poc-python/entrypoint.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+# copy user files to testing directory
 mv "$GITHUB_WORKSPACE/$2" /tests
-
-ERROR_FILE="$(</errors/$1.md)"
-echo ERROR_FILE
-ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
-ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
-ERROR_FILE="${ERROR_FILE//$'\r'/'%0D'}"
-
-echo "::set-output name=error::$ERROR_FILE"
-
+# run tests
 pytest /tests/$1.py
+# Get the result of pytest
+echo Pytest exited $?
+# if test success, move on
+# else comment failure with failure message
+PR_NUMBER=4
+
+curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+-X POST -d '{"body": "Your Message to Comment"}' \
+"https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments"
+
+# ERROR_FILE="$(</errors/$1.md)"
+# echo ERROR_FILE
+# ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
+# ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
+# ERROR_FILE="${ERROR_FILE//$'\r'/'%0D'}"
+
+# echo "::set-output name=error::$ERROR_FILE"
+
 
 
 
