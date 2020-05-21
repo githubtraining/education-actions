87d6ffcc29b4b538d5b2dd76c04439875e148ab9
mattdavis0351 <mattdavis0351@github.com>
2020-05-20 18:39:09 -0700

    anotha try

--- course-envs/actions-poc-python/entrypoint.sh
+++ course-envs/actions-poc-python/entrypoint.sh
@@ -9,12 +9,13 @@ echo Pytest exited $?
 # if test success, move on
 # else comment failure with failure message
 PR_NUMBER=5
+ERROR_FILE="$(</errors/$1.md)"
 
 curl -s -H "Authorization: token ${INPUT_AUTH}" \
--X POST -d '{"body": "Your Message to Comment"}' \
+-X POST -d '{"body": ${ERROR_FILE}}' \
 "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments"
 
-# ERROR_FILE="$(</errors/$1.md)"
+ERROR_FILE="$(</errors/$1.md)"
 # echo ERROR_FILE
 # ERROR_FILE="${ERROR_FILE//'%'/'%25'}"
 # ERROR_FILE="${ERROR_FILE//$'\n'/'%0A'}"
