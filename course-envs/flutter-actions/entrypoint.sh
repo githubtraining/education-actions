b0d7124d2a689c6958cfdc1dd989de7a800405ae
Mike Castner <mjcastner@github.com>
2020-06-01 16:40:33 -0700

    Initial port of Flutter course actions, work in progress

---
+++ course-envs/flutter-actions/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+flutter channel $1;
+flutter upgrade;
+if ["$2" == "web"]; then
+  flutter config --enable-web;
+fi
+if ["$3" == "true"]; then
+  flutter test;
+fi
+flutter build $2;
