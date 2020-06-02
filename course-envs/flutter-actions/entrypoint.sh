f37273442c6b0718df7626b5d57db90926b4cbf3
Mike Castner <mjcastner@github.com>
2020-06-01 17:13:45 -0700

    Fix bash syntax

--- course-envs/flutter-actions/entrypoint.sh
+++ course-envs/flutter-actions/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+ls;
+pwd;
 flutter channel $1;
 flutter upgrade;
-if ["$2" == "web"]; then
+if [[ "$2" == "web" ]]; then
   flutter config --enable-web;
 fi
-if ["$3" == "true"]; then
+if [[ "$3" == "true" ]]; then
   flutter test;
 fi
 flutter build $2;
