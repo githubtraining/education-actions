d84f4ba26d74d03af675d185cd10f47837e71867
Mike Castner <mjcastner@github.com>
2020-06-04 13:44:11 -0700

    Remove cruft, fix syntax for flutter reference

--- course-envs/flutter-actions/entrypoint.sh
+++ course-envs/flutter-actions/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export PATH="$PATH:`pwd`/flutter/bin";
-flutter channel $1;
-flutter upgrade;
+#export PATH="$PATH:`pwd`/flutter/bin";
+./flutter/bin/flutter channel $1;
+./flutter/bin/flutter upgrade;
 if [[ "$2" == "web" ]]; then
-  flutter config --enable-web;
+  ./flutter/bin/flutter config --enable-web;
 fi
 if [[ "$3" == "true" ]]; then
-  flutter test;
+  ./flutter/bin/flutter test;
 fi
-flutter build $2;
+./flutter/bin/flutter build $2;
