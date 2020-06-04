5593153e0862e1b422972ad795314619dd5f1f6c
Mike Castner <mjcastner@github.com>
2020-06-04 13:56:51 -0700

    Clean up entrypoint Flutter commands

--- course-envs/flutter-actions/entrypoint.sh
+++ course-envs/flutter-actions/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ls /
-pwd
-./flutter/bin/flutter channel $1;
-./flutter/bin/flutter upgrade;
+flutter channel $1;
+flutter upgrade;
+
 if [[ "$2" == "web" ]]; then
-  ./flutter/bin/flutter config --enable-web;
+  flutter config --enable-web;
 fi
+
 if [[ "$3" == "true" ]]; then
-  ./flutter/bin/flutter test;
+  flutter test;
 fi
-./flutter/bin/flutter build $2;
+
+flutter build $2;
